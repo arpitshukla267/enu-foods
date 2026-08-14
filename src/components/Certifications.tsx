e6f149fa244b426946a998bfbfcbbb8ff2566410
arpitshukla267 <shuklaarpit440@gmail.com>
2026-08-14 12:48:48 +0530

    fix gapping in certifications

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -35,7 +35,7 @@ export const Certifications: React.FC = () => {
           </h2>
         </div>
 
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-6">
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-x-2 gap-y-4 md:gap-6">
           {CERTIFICATIONS.map((cert) => (
             <div 
               key={cert.id}
